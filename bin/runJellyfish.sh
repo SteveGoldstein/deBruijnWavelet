298f023383a9642a0b70fc621e61e72456ef218c
Steve Goldstein <sgoldstein@wisc.edu>
2021-05-06 21:50:03 -0500

    working version

--- bin/runJellyfish.sh
+++ bin/runJellyfish.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-tarFile=$1;  shift
-jfMerDir=$1; shift
+fastaFile=$1;  shift
+merDir=$1;  shift
+merSize=$1; shift
 CLUSTER=$1;  shift
 PROCESS=$1;  shift
 jfArgs=$@
 
-localCopy=$(basename $tarFile)
-/bin/cp $tarFile $localCopy
-#/bin/scp transfer.chtc.wisc.edu:$tarFile $localCopy
-tar zxf $localCopy
-rm $localCopy
-mkdir mers
 
-for sampleFile in samples/*.fq.gz
-do
-    merFile=$(basename $sampleFile)
-    merFile=${merFile/.fq.gz/.jf}
+merFile=${fastaFile/.fna.gz}
+merFile=$merFile.k$merSize.$CLUSTER.$PROCESS.jf
     
-    echo "bin/jellyfish count $jfArgs -o mers/$merFile"
-    bin/jellyfish count $jfArgs -o mers/$merFile  <(/bin/zcat $sampleFile)
+echo "Running jellyfish count $jfArgs -o $merFile"
+./jellyfish count $jfArgs -o $merFile  <(/bin/zcat $fastaFile)
 
-done
-
-merTarFile=${localCopy/samples.*/jfMers.$CLUSTER.$PROCESS.tgz}
-
-echo "tar zcf $merTarFile mers/*"
-tar zcf $merTarFile mers/*
-
-echo "cp $merTarFile $jfMerDir"
-mv $merTarFile $jfMerDir/
-
-###  bin/runJellyfishOnEachSample.sh $f /mnt/gluster/sgoldstein/Projects/IdentifyGBSSamples/jellyfish/jf-21 "-C -m 21 -t 4 -s 10G" 2 3
+## copy jf output file to /staging 
+mv $merFile $merDir/

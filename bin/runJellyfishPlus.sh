cb34979c3e26c5d8b48100b0789e475ab338c423
Steve Goldstein <sgoldstein@wisc.edu>
2021-05-12 15:12:55 -0500

    remapping output files

--- bin/runJellyfishPlus.sh
+++ bin/runJellyfishPlus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fastaFile=$1;  shift
-merDir=$1;  shift
+outDir=$1;  shift
 merSize=$1; shift
 CPUS=$1;    shift
 CLUSTER=$1;  shift
@@ -11,19 +11,22 @@ jfArgs=$@
 
 
 merFile=${fastaFile/.fna.gz}
-merFile=$merFile.k$merSize.$CLUSTER.$PROCESS.jf
+merFile=$outDir/$merFile.k$merSize.$CLUSTER.$PROCESS.jf
 histoFile=${merFile/.jf/.histo}
 statsFile=${merFile/.jf/.stats}
 
+mkdir $outDir
 echo "Running jellyfish count $jfArgs -o $merFile"
 ./jellyfish count $jfArgs -o $merFile  <(/bin/zcat $fastaFile)
 
 
 echo "Running jellyfish histo and stats"
 ./jellyfish histo --high 1000000 -t $CPUS -o $histoFile $merFile
+/bin/gzip $histoFile
+
 ./jellyfish stats -o $statsFile $merFile
 
-/bin/gzip $histoFile
+
 ## copy jf output file to /staging 
 #mv $merFile $merDir/
 rm $merFile

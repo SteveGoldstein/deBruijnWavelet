ba3842c74cd93cbe6ae2b87dbcb9a8f5d57f71be
Steve Goldstein <sgoldstein@wisc.edu>
2021-05-20 06:49:45 -0500

    WIP: refactoring for splitting FastA files

---
+++ bin/runJellyfishSplit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fastaFile=$1;  shift
+merDir=$1;  shift
+outDir=$1; shift
+merSize=$1; shift
+CPUS=$1; shift
+CLUSTER=$1;  shift
+PROCESS=$1;  shift
+jfArgs=$@
+
+
+merFile=${fastaFile/.fna.gz}
+merFile=$merFile.k$merSize.$CLUSTER.$PROCESS.jf
+    
+echo "Running jellyfish count $jfArgs -o $merFile"
+./jellyfish count $jfArgs -o $merFile  <(/bin/zcat $fastaFile)
+
+## copy jf output file to /staging 
+mv $merFile $merDir/
